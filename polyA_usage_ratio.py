5c908d1c0548295d5146c6dd2ab8129fcc24d53f
marco <marco.marconi@gmail.com>
2014-05-16 19:01:26 +0200

    new DEXseq

--- polyA_usage_ratio.py
+++ polyA_usage_ratio.py
@@ -3,6 +3,7 @@ import sys
 #file = open(sys.stdin, 'r')
 all = {}
 distal = {}
+proximal = {}
 curr = {}
 for line in sys.stdin:
     #print line
@@ -22,8 +23,23 @@ for line in sys.stdin:
         if pos < curr[gene]:    
             distal[gene] = fold, pos
             curr[gene] = pos
+            
+    if not proximal.has_key(gene):
+        proximal[gene] = fold, pos
+        curr[gene] = pos
+    if sense == '-':
+        if pos < curr[gene]:    
+            proximal[gene] = fold, pos
+            curr[gene] = pos
+    else:
+        if pos > curr[gene]:    
+            proximal[gene] = fold, pos
+            curr[gene] = pos
+            
     if not all.has_key(gene):
-        all[gene] = []
+        all[gene] = []        
+    if not all.has_key(gene):
+        all[gene] = []       
     all[gene].append((fold, pos))
     
     
@@ -31,16 +47,18 @@ todistal  = 0
 toproximal = 0
 count = 0.0
 for gene, (fold_d, pos_d) in distal.items():
-    print gene, pos_d, fold_d
+    #print gene, pos_d, fold_d
     if fold_d > 0:
         for (fold_p, pos_p) in all[gene]:
             if fold_p < 0:
-                pass#print gene, pos_p, fold_p, pos_d, fold_d
+                print gene, abs(pos_p - pos_d)
+                break
         todistal += 1 
     else:
         for (fold_p, pos_p) in all[gene]:
             if fold_p > 0:
-                pass#print gene, pos_p, fold_p, pos_d, fold_d
+                print gene, -abs(pos_p - pos_d)
+                break
         toproximal += 1
     count += 1
 #print   toproximal/count, todistal/count            
