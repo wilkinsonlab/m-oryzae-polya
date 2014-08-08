2d1756b95e49e29c135f9407c323e6ec3af263b2
marco <marco.marconi@gmail.com>
2014-08-08 21:11:49 +0200

    gprofiler

---
+++ fasta_extract.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from Bio import SeqIO
+
+fasta_file = sys.argv[1]  # Input fasta file
+extract_file = sys.argv[2] # Input wanted file, one gene name per line
+result_file = sys.argv[3] # Output fasta file
+
+extract = set()
+with open(extract_file) as f:
+    for line in f:
+        line = line.strip()
+        if line != "":
+            extract.add(line)
+
+fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+f =  open(result_file, "w")
+seqs = []
+for seq in fasta_sequences:
+        nuc = seq.id
+	if nuc in extract :
+		seqs.append(seq)
+		if len(seqs) > 10000:
+			SeqIO.write(seqs, f, "fasta")
+			seqs = []
+SeqIO.write(seqs, f, "fasta")
+
+
+
+
